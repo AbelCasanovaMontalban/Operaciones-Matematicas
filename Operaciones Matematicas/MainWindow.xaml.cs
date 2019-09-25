e3f53da3042340e84ee3e63cdb6331be31adb7ed
AbelCasanovaMontalban <abel.casanova@iesdoctorbalmis.com>
2019-09-25 13:35:02 +0200

    Añadir creación calculadora

--- Operaciones Matematicas/MainWindow.xaml.cs
+++ Operaciones Matematicas/MainWindow.xaml.cs
@@ -20,18 +20,137 @@ namespace Operaciones_Matematicas
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool Suma = true;
+        private bool Resta = false;
+        private bool Multiplicacion = false;
+        private bool Division = false;
+
         public MainWindow()
         {
             InitializeComponent();
+            SumaRadioButton.IsChecked = true;
         }
+
+        public void Calcular()
+        {
+
+            int operando1;
+            int operando2;
+            int resultado;
+
+            if (Operando1TextBox.Text.Length>0)
+            {
+                operando1 = Convert.ToInt32(Operando1TextBox.Text);
+            }
+            else
+            {
+                operando1 = 0;
+            }
+
+            if (Operando2TextBox.Text.Length > 0)
+            {
+                operando2 = Convert.ToInt32(Operando2TextBox.Text);
+            }
+            else
+            {
+                operando2 = 0;
+            }
+
+            if (Suma)
+            {
+                resultado = operando1 + operando2;
+                ResultadoTextBox.Text = resultado.ToString();
+            }
+
+            if (Resta)
+            {
+                resultado = operando1 - operando2;
+                ResultadoTextBox.Text = resultado.ToString();
+            }
+
+            if (Multiplicacion)
+            {
+                resultado = operando1 * operando2;
+                ResultadoTextBox.Text = resultado.ToString();
+            }
+
+            if (Division)
+            {
+                if (operando1 == 0 || operando2 == 0)
+                {
+                    if (operando1 == 0 && operando2 == 0)
+                    {
+                        ResultadoTextBox.Text = "0";
+                    }
+                    else
+                    {
+                        ResultadoTextBox.Text = "ERROR";
+                    }
+                    
+                }
+                else
+                {
+                    resultado = operando1 / operando2;
+                    ResultadoTextBox.Text = resultado.ToString();
+                }
+                
+            }
+
+
+        }
+
 
         private void Operando1TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            if (SumaRadioButton.IsChecked == "RadioButton_Checked")
-            {
+            Calcular();
+        }
 
-            }
-            ResultadoTextBox.Text=Operando1TextBox.Text
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Operando1TextBox.Text = "0";
+            Operando2TextBox.Text = "0";
+            Calcular();
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            Suma = true;
+            Multiplicacion = false;
+            Resta = false;
+            Division = false;
+            Calcular();
+        }
+
+        private void RestaRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            Suma = false;
+            Multiplicacion = false;
+            Resta = true;
+            Division = false;
+            Calcular();
+        }
+
+        private void MultiplicacionRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            Suma = false;
+            Multiplicacion = true;
+            Resta = false;
+            Division = false;
+            Calcular();
+        }
+
+        private void DivisionRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            Suma = false;
+            Multiplicacion = false;
+            Resta = false;
+            Division = true;
+            Calcular();
+        }
+
+        private void Operando2TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Calcular();
         }
     }
 }
